80eafb85a124058c5578b126b4a590bfb675b49c
Ken980 <kenboy9800@gmail.com>
2025-09-27 21:38:50 +0700

    update project

--- Computer Graphics/test/test.cpp
+++ Computer Graphics/test/test.cpp
@@ -5,17 +5,7 @@
 #include <fstream>
 #include <string>
 
-// Matrix operations - Students implement these
-void Zero(int Matrix[3][3]) {
-    
-    Matrix[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
-}
-
-
 int main() {
-    int Matrix[3][3];
-    std::cout << Matrix[3][3] << std::endl;
-    Zero(Matrix);
-    std::cout << Matrix[3][3] << std::endl;
+    std::cout << "hi" << std::endl;
     return 0;
 }
